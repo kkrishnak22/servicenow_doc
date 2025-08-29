649dc8c4541992e8ece991fe7c63502aa272aa50
kkrishnak22 <97334231+kkrishnak22@users.noreply.github.com>
2025-08-29 23:44:15 +0530

    Update addUsersToCC.js

--- Scripts/Email Scripts/addUsersToCC.js
+++ Scripts/Email Scripts/addUsersToCC.js
@@ -1,1 +1,11 @@
+var emailArr = [];
 
+emailArr.push({
+			email: current.parent.parent.account_manager.email.toString(),  // Email
+			name: current.parent.parent.account_manager.name.toString() // Name
+});
+
+for (var p = 0; p < emailArr.length; p++) {
+		var temp = emailArr[p];
+		email.addAddress('cc', temp.email.toString(), temp.name);
+}
