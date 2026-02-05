131978a51d9ad43bf9953be418e34c3b5a6ca1b2
kkrishnak22 <krishnakumar.j22@gmail.com>
2026-02-05 13:33:35 +0530

    Navigation

--- Scripts/Custom Utils/Navigation/redirectToNewPage.js
+++ Scripts/Custom Utils/Navigation/redirectToNewPage.js
@@ -1,11 +1,16 @@
 
-
+// 1st
 var currentURL = gs.getProperty('glide.servlet.uri');
 
 action
     .setRedirectURL
     (
-        currentURL
-        + 'x_adga_pr_automa_0_pr_line_items.do?sys_id=-1%26sys_param_query=parent='
+        currentURL 
+        + 'x_adga_pr_automa_0_pr_line_items.do?sysparm_stack=x_adga_pr_automa_0_pr_line_items.do&sys_id=-1&sysparm_query=parent='
         + current.sys_id
+        + '^email_address=abc@gmail.com'
+
     );
+
+// 2nd
+action.setRedirectURL(new GlideRecord('table_name'));
